fe29ddf08ed73ae5d267ead8e33b76f041472e6e
sayakbiswas <sayak90@gmail.com>
2017-04-22 09:54:21 -0400

    Changed signature of saveModel to take saver as argument

--- chatbot_seq2seq.py
+++ chatbot_seq2seq.py
@@ -64,7 +64,7 @@ class Batch:
 		self.targetSeqs = []
 		self.weights = []
 
-def saveModel():
+def saveModel(saver):
 	print('Saving model checkpoint...')
 	model_name = 'model_' + str(globalStep) + '.ckpt'
 	if globalStep == 30:
@@ -373,10 +373,10 @@ def main():
 							print("Loss %.2f" % (loss))
 							print("Perplexity %.2f" % (perplexity))
 						if globalStep % 10 == 0:
-							saveModel()
+							saveModel(saver)
 			except (KeyboardInterrupt, SystemExit):
 				print('Exiting')
-			saveModel()
+			saveModel(saver)
 			sess.close()
 
 if __name__ == "__main__":
